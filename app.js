5f2d92349f32a6432b623d9a968daf7ce13cc1ab
Xiao Hu <xiao@squareup.com>
2019-05-23 17:15:21 -0700

    Add basic server template.

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const squareConnect = require('square-connect');
+
+const app = express();
+const port = 3000;
+const accessToken = 'REPLACE_ME';
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname));
+
+// Set Square Connect credentials
+const defaultClient = squareConnect.ApiClient.instance;
+
+// Configure OAuth2 access token for authorization: oauth2
+const oauth2 = defaultClient.authentications['oauth2'];
+oauth2.accessToken = accessToken;
+
+app.post('/process-payment', function(req,res,next){
+	const request_params = req.body;
+
+	const idempotency_key = require('crypto').randomBytes(64).toString('hex');
+
+	// Charge the customer's card
+	const transactions_api = new squareConnect.TransactionsApi();
+	const request_body = {
+		card_nonce: request_params.nonce,
+		amount_money: {
+			amount: 100, // $1.00 charge
+			currency: 'USD'
+		},
+		idempotency_key: idempotency_key
+  };
+	transactions_api.charge(config.squareLocationId, request_body).then(function(data) {
+		const json= JSON.stringify(data);
+		res.render('process-payment', {
+			'title': 'Payment Successful',
+			'result': json
+		});
+	}, function(error) {
+		res.render('process-payment', {
+			'title': 'Payment Failure',
+			'result': error.response.text
+		});
+	});
+});
+
+app.listen(port, () => console.log(`listening on - http://localhost:${port}`))
